d54c4f7af191cec8adf212f575ad0e5a84db714e
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-09-02 19:01:45 -0400

    init

---
+++ src/main/java/com/github/ka4ok85/cryptocomparestreamconsumer/CryptoCompareStreamConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.github.ka4ok85.cryptocomparestreamconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCompareStreamConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoCompareStreamConsumerApplication.class, args);
+	}
+}
