1e7368f123d2c48745fdcd067e7c2de33d59ebdb
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-09-05 18:10:47 -0400

    enabled reactive mongo repos

--- src/main/java/com/github/ka4ok85/cryptocomparestreamconsumer/CryptoCompareStreamConsumerApplication.java
+++ src/main/java/com/github/ka4ok85/cryptocomparestreamconsumer/CryptoCompareStreamConsumerApplication.java
@@ -2,8 +2,10 @@ package com.github.ka4ok85.cryptocomparestreamconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class CryptoCompareStreamConsumerApplication {
 
 	public static void main(String[] args) {
