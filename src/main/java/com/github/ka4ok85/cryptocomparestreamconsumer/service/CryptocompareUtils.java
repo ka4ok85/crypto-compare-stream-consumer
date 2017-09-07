ee411a0f58b0977781350658ac9bb16575dc5e5d
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-09-06 22:21:02 -0400

    added logic for proper filtering for incoming messages: ignoring messages w/o required fields and w/o rate change

--- src/main/java/com/github/ka4ok85/cryptocomparestreamconsumer/service/CryptocompareUtils.java
+++ src/main/java/com/github/ka4ok85/cryptocomparestreamconsumer/service/CryptocompareUtils.java
@@ -10,29 +10,16 @@ import com.github.ka4ok85.cryptocomparestreamconsumer.entity.LiveRate;
 public class CryptocompareUtils {
 
 	public static final String separator = "~";
-
-	// public static final String acceptedMask = "ce9";
 	/*
-	 * public static LiveRate stringToLiveRate(String message) { String[] items
-	 * = message.split(CryptocompareUtils.separator);
-	 *
-	 * 
-	 * we accept only certain message types if you want to accept all message
-	 * types you will need to parse mask see
-	 * https://cryptoqween.github.io/streamer/ccc-streamer-utilities.js
-	 *
-	 * if (items.length == 13 &&
-	 * items[12].equals(CryptocompareUtils.acceptedMask)) { LiveRate newLiveRate
-	 * = new LiveRate(items[1], items[2], items[3], Byte.parseByte(items[4]),
-	 * Double.parseDouble(items[5]), Long.parseLong(items[6]),
-	 * Double.parseDouble(items[7]), Double.parseDouble(items[8]),
-	 * Long.parseLong(items[9]), Double.parseDouble(items[10]),
-	 * Double.parseDouble(items[11]));
-	 * 
-	 * return newLiveRate; }
-	 * 
-	 * return null; }
+	 * Change Flag 4 means no rate change. We can ignore these as we will catch
+	 * up volume changes on next "valid" message
 	 */
+	public static final String unacceptedFlag = "4";
+	/*
+	 * Type flag = 5 is for rates
+	 */
+	public static final String acceptedType = "5";
+
 	public static String hexStringToBinaryString(String hex) {
 		return String.format("%16s", Integer.toBinaryString(Integer.parseInt(hex, 16))).replace(' ', '0');
 	}
@@ -43,14 +30,15 @@ public class CryptocompareUtils {
 		 * useless for us at this point every record by default has type,
 		 * market, fromSymbol, toSymbol and flag
 		 */
-		if (mask.charAt(15) == '1' && mask.charAt(12) == '1' && mask.charAt(5) == '1' && mask.charAt(4) == '1') {
+
+		if (mask.charAt(15) == '1' && mask.charAt(5) == '1' && mask.charAt(4) == '1') {
 			return true;
 		}
 
 		return false;
 	}
 
-	public static LiveRate stringArrayToLiveRate(String[] items) {
+	public static LiveRate stringArrayToLiveRate(String[] items, String mask) {
 		/*
 		 * here we replace lastUpdated value with current timestamp. Usually
 		 * difference is within 3 seconds range (depending on the network
@@ -59,20 +47,38 @@ public class CryptocompareUtils {
 		 * we can't rely on lastUpdated value provided by service since it
 		 * included very rarely and that means we would skip 90%+ of all records
 		 */
-		LiveRate newLiveRate = new LiveRate(items[1], items[2], items[3], Byte.parseByte(items[4]),
-				Double.parseDouble(items[5]), Instant.now().getEpochSecond(), Double.parseDouble(items[7]),
-				Double.parseDouble(items[8]));
-		/*
-0 5
-1 CCCAGG
-2 BTC
-3 USD
-4 1
-5 4415.43
-6 156375.45963145923
-7 665018292.0452391
-8 c01
-*/
+
+		// always provided at fixed position
+		String exchangeName = items[1];
+		String fromCurrency = items[2];
+		String toCurrency = items[3];
+		byte flag = Byte.parseByte(items[4]);
+
+		// position may vary depending on mask
+		double price = 0;
+		long lastUpdate = Instant.now().getEpochSecond();
+		double volume24h = 0;
+		double volume24hTo = 0;
+
+		int fixedOffset = 5; // 4 fixed fields + type
+		int offset = 0;
+		for (int i = mask.length() - 1; i > 0; i--) {
+			if (mask.charAt(i) == '1') {
+				if (i == 15) {
+					price = Double.parseDouble(items[fixedOffset + offset]);
+				} else if (i == 5) {
+					volume24h = Double.parseDouble(items[fixedOffset + offset]);
+				} else if (i == 4) {
+					volume24hTo = Double.parseDouble(items[fixedOffset + offset]);
+				}
+
+				offset++;
+			}
+		}
+
+		LiveRate newLiveRate = new LiveRate(exchangeName, fromCurrency, toCurrency, flag, price, lastUpdate, volume24h,
+				volume24hTo);
+
 		return newLiveRate;
 	}
 }
