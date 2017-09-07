a8d3d5121166dfe4f8709fd50a7e38a7bb968172
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-09-06 20:28:39 -0400

    added Tailable support to Reactive Mongo LiveRate repository

--- src/main/java/com/github/ka4ok85/cryptocomparestreamconsumer/repository/LiveRateRepository.java
+++ src/main/java/com/github/ka4ok85/cryptocomparestreamconsumer/repository/LiveRateRepository.java
@@ -1,18 +1,15 @@
 package com.github.ka4ok85.cryptocomparestreamconsumer.repository;
 
 import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
 
 import com.github.ka4ok85.cryptocomparestreamconsumer.entity.LiveRate;
 
 import reactor.core.publisher.Flux;
 
-@Document(collection = "live_rates")
 public interface LiveRateRepository extends ReactiveMongoRepository<LiveRate, ObjectId> {
 
-	public Flux<LiveRate> findByExchangeName(final String firstName);
-
+	@Tailable
 	public Flux<LiveRate> findByLastUpdate(final long lastUpdate);
-
 }
