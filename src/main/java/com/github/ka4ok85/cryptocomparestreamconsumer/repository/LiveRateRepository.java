8d81d50891ee42be1b78a2df347b37b7c9ca18e0
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-09-08 22:27:54 -0400

    added query for by FromCurrency

--- src/main/java/com/github/ka4ok85/cryptocomparestreamconsumer/repository/LiveRateRepository.java
+++ src/main/java/com/github/ka4ok85/cryptocomparestreamconsumer/repository/LiveRateRepository.java
@@ -11,5 +11,5 @@ import reactor.core.publisher.Flux;
 public interface LiveRateRepository extends ReactiveMongoRepository<LiveRate, ObjectId> {
 
 	@Tailable
-	public Flux<LiveRate> findByLastUpdate(long lastUpdate);
+	public Flux<LiveRate> findByFromCurrency(String fromCurrency);
 }
